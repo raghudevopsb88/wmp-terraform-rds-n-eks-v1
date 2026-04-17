d9ac16bbf7e3c583449af2a82036a809cec7a744
Raghu K <raghuk.vit@gmail.com>
2026-04-17 07:42:57 +0530

    WMP scripts

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -9,3 +9,23 @@ resource "null_resource" "kube-config" {
   }
 
 }
+
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
+
+resource "helm_release" "argocd" {
+  name       = "argocd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+
+  set = [
+    {
+      name  = "server.service.LoadBalancer"
+      value = "enabled"
+    }
+  ]
+}
+
